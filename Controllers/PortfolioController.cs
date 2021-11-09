cd160e879ffa43ae2115aee331946339600f179f
michael <carl.michael.sjogren@gmail.com>
2021-11-09 11:26:28 +0100

    basic scaffolding of project

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MVCBasics.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
