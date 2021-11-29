90b0406408cd1e05c992cf8af2f745faac2c43a9
michael <carl.michael.sjogren@gmail.com>
2021-11-29 14:17:21 +0100

    basic entity framework core connection established

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVCBasics.DataAccess;
 using MVCBasics.Models;
 using MVCBasics.Models.Interfaces;
 
@@ -16,8 +14,21 @@ namespace MVCBasics
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<PeopleContext>(options =>
+            {
+                options.UseMySql(connectionString , ServerVersion.AutoDetect(connectionString));
+            });
+
             services.AddScoped<IProjectsRepository, ProjectsRepository>();
             services.AddScoped<IPeopleRepository, PeopleRepository>();
             services.AddControllersWithViews();
