9703b42b36a3037ab637ebf6032df271c0460057
michael <carl.michael.sjogren@gmail.com>
2021-12-01 14:15:38 +0100

    d

--- ViewModels/CityViewModel.cs
+++ ViewModels/CityViewModel.cs
@@ -4,6 +4,6 @@ namespace MVCBasics.ViewModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public PeopleViewModel PeopoleVm { get; set; }
+        public string CountryName { get; set; }
     }
 }
