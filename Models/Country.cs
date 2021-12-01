9f2c6949b646b27d6b9ac505327826c031379a44
michael <carl.michael.sjogren@gmail.com>
2021-12-01 14:15:53 +0100

    empty list

--- Models/Country.cs
+++ Models/Country.cs
@@ -7,6 +7,6 @@ namespace MVCBasics.Models
     {
         [Required] [Key] public int Id { get; set; }
         [Required] [MaxLength(100)] public string Name { get; set; }
-        public List<City> Cities { get; set; }
+        public List<City> Cities { get; set; } = new List<City>();
     }
 }
