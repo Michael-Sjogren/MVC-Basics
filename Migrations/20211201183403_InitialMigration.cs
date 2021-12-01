1d5c6eee6d25a07ed531921495fa1600580e7f66
michael <carl.michael.sjogren@gmail.com>
2021-12-01 19:36:05 +0100

    migration files

---
+++ Migrations/20211201183403_InitialMigration.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCBasics.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Language",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Language", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PhoneNumber = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_People_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PersonLanguages",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    LanguageId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonLanguages", x => new { x.PersonId, x.LanguageId });
+                    table.ForeignKey(
+                        name: "FK_PersonLanguages_Language_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Language",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonLanguages_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Sweden" });
+
+            migrationBuilder.InsertData(
+                table: "Language",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "French" },
+                    { 2, "German" },
+                    { 3, "Swedish" },
+                    { 4, "Norwegian" },
+                    { 5, "Japanese" },
+                    { 6, "English" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "CountryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Göteborg" },
+                    { 2, 1, "Stockholm" },
+                    { 3, 1, "Malmö" },
+                    { 4, 1, "Kiruna" },
+                    { 5, 1, "Helsingborg" },
+                    { 6, 1, "Uppsala" },
+                    { 7, 1, "Karlstad" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "People",
+                columns: new[] { "Id", "CityId", "Name", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 6, 1, "Lena", "555-893-321" },
+                    { 11, 1, "Eva", "555-321-555" },
+                    { 3, 2, "Sten", "555-321-468" },
+                    { 12, 4, "Hamid", "555-873-321" },
+                    { 1, 5, "Michael Sjögren", "555-322-31" },
+                    { 7, 5, "Lisbeth", "555-321-567" },
+                    { 8, 5, "Niklas", "555-321-324" },
+                    { 2, 6, "Anders", "555-321-324" },
+                    { 4, 6, "Leonard", "555-897-321" },
+                    { 5, 7, "Amir", "555-893-321" },
+                    { 9, 7, "Stefan", "555-783-321" },
+                    { 10, 7, "Lina", "555-321-645" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PersonLanguages",
+                columns: new[] { "LanguageId", "PersonId" },
+                values: new object[,]
+                {
+                    { 5, 6 },
+                    { 4, 11 },
+                    { 5, 3 },
+                    { 2, 12 },
+                    { 6, 12 },
+                    { 1, 1 },
+                    { 3, 1 },
+                    { 6, 1 },
+                    { 1, 7 },
+                    { 4, 8 },
+                    { 3, 2 },
+                    { 1, 4 },
+                    { 3, 5 },
+                    { 3, 9 },
+                    { 1, 10 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_CityId",
+                table: "People",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonLanguages_LanguageId",
+                table: "PersonLanguages",
+                column: "LanguageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonLanguages");
+
+            migrationBuilder.DropTable(
+                name: "Language");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
