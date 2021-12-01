637a33603af0065fed4847f3a8dae168e39e2e51
michael <carl.michael.sjogren@gmail.com>
2021-12-01 14:18:18 +0100

    country repository interface defined

--- Models/Interfaces/ICountryRepository.cs
+++ Models/Interfaces/ICountryRepository.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using MVCBasics.ViewModels;
+
 namespace MVCBasics.Models.Interfaces
 {
     public interface ICountryRepository
     {
-        
+        public List<Country> GetAllCountries();
+        public Country GetCountryById(int id);
+        public Country GetCountryByName(string name);
+        public void DeleteCountry(int id);
+
+        public void CreateCountry(CreateCountryViewModel vm);
     }
 }
