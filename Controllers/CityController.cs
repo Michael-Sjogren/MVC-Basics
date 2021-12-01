2c97b334febcc1756c491c0432c268e66a2a9fa5
michael <carl.michael.sjogren@gmail.com>
2021-12-01 14:15:16 +0100

    basic routes and access to repositories needed.

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -1,13 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using MVCBasics.Models.Interfaces;
+using MVCBasics.ViewModels;
 
 namespace MVCBasics.Controllers
 {
     public class CityController : Controller
     {
-        // GET
+        private readonly ICitiesRepository _citiesRepository;
+        private readonly ICountryRepository _countryRepository;
+
+        public CityController(ICitiesRepository citiesRepository, ICountryRepository countryRepository)
+        {
+            _citiesRepository = citiesRepository;
+            _countryRepository = countryRepository;
+        }
+        
+        [HttpGet]
+        [Route("/Cities/")]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Countries = _countryRepository.GetAllCountries();
+            return View(_citiesRepository.GetAllCities());
+        }
+
+        public IActionResult Create(CreateCityViewModel model)
+        {
+            if (!ModelState.IsValid) return RedirectToAction(nameof(Index));
+            _citiesRepository.CreateCity(model);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _citiesRepository.DeleteCity(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
