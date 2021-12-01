66178c96625e13ccb254c78e4bc6e4e471fb3d28
michael <carl.michael.sjogren@gmail.com>
2021-12-01 14:16:50 +0100

    country controller, and repository for data access

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -1,13 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using MVCBasics.Models.Interfaces;
+using MVCBasics.ViewModels;
 
 namespace MVCBasics.Controllers
 {
     public class CountryController : Controller
     {
-        // GET
+        private readonly ICountryRepository _countryRepository;
+        public CountryController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+        
+        [HttpGet]
+        [Route("/Countries/")]
         public IActionResult Index()
         {
-            return View();
+            return View(_countryRepository.GetAllCountries());
+        }
+        
+        [HttpPost]
+        public IActionResult Create(CreateCountryViewModel vm)
+        {
+            if (!ModelState.IsValid) 
+                return RedirectToAction(nameof(Index));
+            _countryRepository.CreateCountry(vm);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _countryRepository.DeleteCountry(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Models/Repository/CountryRepository.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using MVCBasics.DataAccess;
+using MVCBasics.Models.Interfaces;
+using MVCBasics.ViewModels;
+
+namespace MVCBasics.Models.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly AppDbContext _context;
+        public CountryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public List<Country> GetAllCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountryById(int id)
+        {
+            return _context.Countries.Find(id);
+        }
+
+        public Country GetCountryByName(string name)
+        {
+            return _context.Countries.First(c => c.Name.Equals(name));
+        }
+
+        public void DeleteCountry(int id)
+        {
+            _context.Countries.Remove(GetCountryById(id));
+            _context.SaveChanges();
+        }
+
+        public void CreateCountry(CreateCountryViewModel vm)
+        {
+            _context.Countries.Add(
+                new Country(){Name = vm.Name}
+            );
+            _context.SaveChanges();
+        }
+    }
+}
