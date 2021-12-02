f4249c8b3c5397ffde87326a1ae2b9f0cc7de485
michael <carl.michael.sjogren@gmail.com>
2021-12-02 13:18:47 +0100

    corrected datatype to be text instead of time for language name.

--- ViewModels/CreateLanguageViewModel.cs
+++ ViewModels/CreateLanguageViewModel.cs
@@ -4,6 +4,6 @@ namespace MVCBasics.ViewModels
 {
     public class CreateLanguageViewModel
     {
-        [Required] [DataType(DataType.Time)] [MaxLength(150)] public string Name { get; set; }
+        [Required] [DataType(DataType.Text)] [MaxLength(150)] public string Name { get; set; }
     }
 }
